953ab1d32b3a320060fccefa98c23bbbc3a4c689
AlanHTL <165019180+AlanHTL@users.noreply.github.com>
2025-04-08 19:36:20 +0800

    update api

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ os.environ.clear()
 dotenv.load_dotenv(override=True)
 
 # Get the API key
-API_KEY = st.secrets['API_KEY'] | os.getenv("API_KEY")
+API_KEY = st.secrets['API_KEY']
 
 # Verify API key is loaded correctly
 if not API_KEY:
